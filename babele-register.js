2e5f86c6409eb5a89c32ab87599ca8ae40e5b5af
Deathkael83 <135058800+Deathkael83@users.noreply.github.com>
2025-11-26 22:56:17 +0100

    Update babele-register.js

--- babele-register.js
+++ babele-register.js
@@ -1,12 +1,5 @@
-Hooks.once('init', () => {
-  if (typeof Babele === 'undefined') {
-    console.error("Babele non è attivo: impossibile registrare la traduzione RNHD.");
-    return;
-  }
-
   Babele.get().register({
     module: 'wfrp4e-rnhd',  // id del modulo ORIGINALE
     lang: 'it',
     dir: 'translations'     // cartella del tuo wfrp4e-rnhd-it
   });
-});
